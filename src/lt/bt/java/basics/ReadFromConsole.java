ebe23da32a453338d196bda22f6f2f71af6bd390
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-11-13 20:57:58 +0200

    samples for reading from console and printing to the scree

---
+++ src/lt/bt/java/basics/ReadFromConsole.java
@@ -0,0 +1,15 @@
+package lt.bt.java.basics;
+
+import java.util.Scanner;
+
+public class ReadFromConsole {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name: ");
+		Scanner reader= new Scanner(System.in);		
+		String name = reader.nextLine();
+		reader.close();
+		System.out.println("Hello, " + name + "!");
+	}
+
+}
