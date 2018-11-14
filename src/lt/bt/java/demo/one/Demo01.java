542d7e1336a236f512f9d6594c40d9aac8f29e55
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-11-14 09:47:15 +0200

    Some demos.

---
+++ src/lt/bt/java/demo/one/Demo01.java
@@ -0,0 +1,19 @@
+package lt.bt.java.demo.one;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		int sk1 = -3;
+		int sk2 = 3;
+		int ats = sk1 + sk2;
+		
+		if(ats > 3 && ats < 10){
+			System.out.println("Kazkas blogai");			
+		} else if (ats == 3 || ats == 0){
+			System.out.println("atsakymas: " + ats);
+		} else {
+			System.out.println("netiketas rezultatas: " + ats);
+		}
+	}
+
+}
---
+++ src/lt/bt/java/demo/one/Demo02.java
@@ -0,0 +1,17 @@
+package lt.bt.java.demo.one;
+
+import java.util.Scanner;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		System.out.println("Iveskite savo varda: ");
+		Scanner reader= new Scanner(System.in);		
+		String name = reader.nextLine();
+		
+		System.out.println("Labas," + name + "!");
+		reader.close();
+
+	}
+
+}
