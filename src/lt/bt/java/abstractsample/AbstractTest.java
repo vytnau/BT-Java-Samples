9f05e61006594bf89747154a203e314f7252063d
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-11-27 23:24:41 +0200

    Updated farm sample.

--- src/lt/bt/java/abstractsample/AbstractTest.java
+++ src/lt/bt/java/abstractsample/AbstractTest.java
@@ -1,5 +1,11 @@
 package lt.bt.java.abstractsample;
 
+import lt.bt.java.abstractsample.farms.CowFarm;
+import lt.bt.java.abstractsample.farms.Farm;
+import lt.bt.java.abstractsample.farms.PigFarm;
+import lt.bt.java.abstractsample.farms.data.Food;
+import lt.bt.java.tasks.task8.data.Cow;
+
 public class AbstractTest {
 
 	//Abstrakti klase ferma
@@ -9,7 +15,13 @@ public class AbstractTest {
 	//kiekviena fermos klase kaupia pasaro atsargas ziemai
 	//uzduotis: Parasyti programa, kuri suskaiciuotu kiek dienu uzteks pasaro
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Farm cowFarm = new CowFarm();
+		Farm pigFarm = new PigFarm();
+		
+		cowFarm.addAnimal(new Cow());
+		cowFarm.addFood(new Food());
+		pigFarm.addAnimal(new Cow());
+		pigFarm.addFood(new Food());
 
 	}
 
--- src/lt/bt/java/abstractsample/farms/Farm.java
+++ src/lt/bt/java/abstractsample/farms/Farm.java
@@ -7,9 +7,38 @@ public abstract class Farm {
 	protected Animal[] animals;
 	protected Food[] foods;
 	protected int workersCount;
-	protected int animalsCount;
-	
+	protected int animalsCount = 0;
+
 	protected abstract void feedAnimals();
-	
-	
+
+	public void addAnimal(Animal animal) {
+		if (animals == null) {
+			animals = new Animal[1];
+			animals[0] = animal;
+			animalsCount++;
+		} else {
+			Animal[] tmp = new Animal[animalsCount + 1];
+			for (int i = 0; i < animalsCount; i++) {
+				tmp[i] = animals[i];
+			}
+			tmp[animalsCount] = animal;
+			animalsCount++;
+			animals = tmp;
+		}
+	}
+
+	public void addFood(Food food) {
+		if (foods == null) {
+			foods = new Food[1];
+			foods[0] = food;			
+		} else {
+			int foodsCount = foods.length;
+			Food[] tmp = new Food[foodsCount + 1];
+			for (int i = 0; i < foodsCount; i++) {
+				tmp[i] = foods[i];
+			}
+			tmp[foodsCount] = food;
+			foods = tmp;
+		}
+	}
 }
