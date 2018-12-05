dc8bfbacfd5716d0178bd3b810a6f95407a7fac4
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-12-05 09:50:43 +0200

    Print numbers

--- src/lt/bt/java/basics/HelloWorld.java
+++ src/lt/bt/java/basics/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println(args[0]);
 		System.out.println("Hello world");
+		
+		for(int i = 0; i < 10; i++){
+			System.out.println(i);
+		}
 	}
 
 }
