1cf0b197202119e90ca787625eba4090781bf277
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-12-05 10:32:01 +0200

    more changes

--- src/lt/bt/java/basics/HelloWorld.java
+++ src/lt/bt/java/basics/HelloWorld.java
@@ -6,15 +6,11 @@ public class HelloWorld {
 		System.out.println(args[0]);
 		System.out.println("Hello world");
 		
-		for(int i = 0; i < 10; i++){
+		for(int i = 5; i < 10; i++){
 			System.out.println(i);
 		}
-		
-		
-		
-		
-		
-		for (int i = 10; i > 0 ; i++){
+			
+		for (int i = 20; i > 0 ; i++){
 			System.out.println(i);
 		}
 		
