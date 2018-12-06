dfacf8649506acb3e4836976bd6ad81adfd6b413
Vytautas Naudžius <Vytautas.Naudzius@gmail.com>
2018-12-06 11:39:19 +0200

    List example for Padanga

---
+++ src/lt/bt/java/task/padangos/uzdList/RatasWrap.java
@@ -0,0 +1,26 @@
+package lt.bt.java.task.padangos.uzd;
+
+import java.util.List;
+
+//Tarpine klase duomenim pernesti is funkcijos
+public class RatasWrap {
+	private Padanga ieskomasRatas;
+	private List<Padanga> turimiRatai;
+
+	public Padanga getIeskomasRatas() {
+		return ieskomasRatas;
+	}
+
+	public void setIeskomasRatas(Padanga ieskomasRatas) {
+		this.ieskomasRatas = ieskomasRatas;
+	}
+
+	public List<Padanga> getTurimiRatai() {
+		return turimiRatai;
+	}
+
+	public void setTurimiRatai(List<Padanga> turimiRatai) {
+		this.turimiRatai = turimiRatai;
+	}
+
+}
